e00a68fc0d11e9e133c5ebdcc91a146633dbcbf3
MichaelBicaldo <145413941+MichaelBicaldo@users.noreply.github.com>
2023-09-19 11:07:15 +0800

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+# 
+# 
+# 
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+# 
+# 
+
+# # Takeaways of this course
+# 
+#  1. How to Describe the components of a Data Scientist's toolkit and list various tool categories.
+#  2. What are the relevant features if Open Source Tools
+#  3. examples of commercial and cloud-based data science tools.
+#  
+#  
+#  # Objectives
+#  
+#  unordered list:
+#  
+#  - commonly used libraries used by Data Scientists
+#  - Popular languages that Data Scientists
+#  - Arithmetic expression examples
+#  
+#  
+
+# # Some of the Popular languages that Data Scientists use are:
+# 
+# ordered list
+# 
+# 1. Python
+# 2. R
+# 3. SQL
+# 4. Java
+# 5. Julia
+# 6. Scala
+# 7. MATLAB
+# 8. Javascript
+# 9. C++
+# 10. Ruby
+
+# 
+# # Some of the commonly used libraries used by Data Scientists include:
+# 
+# ordered list:
+# 
+# 1. Pandas
+# 2. Matplotlib
+# 3. TensorFlow
+# 4. Numply
+# 5. Scikit-learn
+# 6. SciPy
+# 7. PyTorch
+# 8. Keras
+# 9. Theano
+# 10. Plotly
+# 
+
+# 
+# 
+# | Data Science Tools |
+# |--------------------|
+# |Pandas              |
+# |Weka                |
+# |Scrapy              |
+# 
+
+# # Arithmetic expression examples
+# 
+#    Below are a few examples of evaluating arithmetic expressions in Python
+#    
+#  This a simple arithmetic expression to mutiply then add integers
+
+# In[8]:
+
+
+(3*4)+5
+
+
+# # Convert Minutes to Hour
+# This will convert 200 minutes to hours by diving by 60
+
+# In[12]:
+
+
+200/60
+
+
+# # Author
+# 
+# Michael N. Bicaldo
+# 
+# 
+
+# In[ ]:
+
+
+
+
